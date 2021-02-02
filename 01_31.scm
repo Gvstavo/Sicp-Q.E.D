f3b7928fcdd4ddc3cf806648fd9c37d8a0612a72
Gvstavo <gustavopaivads@gmail.com>
2021-02-01 23:20:41 -0300

    > 01_31, partial

---
+++ 01_31.scm
@@ -0,0 +1,39 @@
+; a product define
+
+(define (product term a next b )
+	(if (> a b)
+		1
+	(* (term a) (product term (next a) next b)))
+)
+
+(define (inc x) (+ x 1))
+
+(define (identity x) x)
+
+
+(define (factorial n)
+	(product identity 1 inc n)
+)
+
+
+;(display (factorial 3))
+
+;b iterative product
+
+(define (product-iter term a next b)
+	(define (iter a result)
+
+		(if (> a  b)
+			result
+			(iter (next a) (* result a ))
+		)
+	)
+	(iter a 1)
+)
+
+
+(define (factorial-iter n)
+	(product-iter identity 1 inc n)
+)
+
+;(display (factorial-iter 3))
