ed78f32202fbfc8370e2e38181d4fa05715f6777
Gvstavo <gustavopaivads@gmail.com>
2021-06-05 03:57:21 -0300

    > 1.45

--- 01_45.scm
+++ 01_45.scm
@@ -2,6 +2,16 @@
 	(/ (+ a b) 2)
 )
 
+(define (compose f g)(lambda (x) (f (g x)) ))
+
+
+(define (repeated f n)
+	(if (= n 1)
+		f
+	 (repeated (compose f f) (- n 1))
+	)
+)
+
 (define tolerance 0.00001)
 
 (define (average-damp f)
@@ -21,3 +31,23 @@
 	
 		(try first-guess)
 )
+
+
+(define (expr x n)
+	(if (= n 1)
+		x 
+	(* x (expr x (- n 1))))
+)
+
+(define (logarithm n k)(/ (log k) (log n)))
+
+(define (nth x n)
+	(fixed-point 
+		((repeated average-damp (floor (logarithm 2 n))) (lambda (y) (/ x (expr y (- n 1)))) ) 1.0
+	)
+)
+
+
+;(lambda (y) (/ x (expr y (- n 1))))
+
+;(display (logarithm 5 25))
