5eeb8af78be5c4aa323a3b8eaba910703550dd73
Gvstavo <gustavopaivads@gmail.com>
2021-07-10 21:02:26 -0300

    > 2.29

--- chapter_II/02_29.scm
+++ chapter_II/02_29.scm
@@ -8,13 +8,14 @@
 )
 
 (define (right-branch mobile)
-	(car mobile)
+	(car (cdr mobile))
+	;(car mobile)
 )
 
 (define (left-branch mobile)
-	(car (cdr mobile))
+	(car mobile)
+	;(car (cdr mobile))
 )
-
 
 
 (define (branch-length branch)
@@ -41,19 +42,50 @@
 
 (define (balanced mobile)
 
+	(define (branch-weight branch n)
+		(if (pair? (branch-structure branch))
+			(total-weight (branch-structure branch))
+			(+ n (branch-structure branch))
+		)
+	)
+
+	(define (branch-balanced branch)
+		(if (pair? (branch-structure branch))
+			(balanced (branch-structure branch))
+			#t
+		)
+	)
+
+
+
+	(and
+		(=
+			(*(car (left-branch mobile)) (branch-weight (left-branch mobile) 0) )
+			(*(car (right-branch mobile)) (branch-weight (right-branch mobile) 0) )
+		)
+		(branch-balanced (left-branch mobile))
+		(branch-balanced (right-branch mobile))
+	)
 
 )
 
 (define a (make-mobile (make-branch 2 3) (make-branch 2 3)))	
 (define b (make-mobile (make-branch 2 3) (make-branch 4 5)))
 (define c (make-mobile (make-branch 5 a) (make-branch 3 b)))
+(define d (make-mobile (make-branch 10 a) (make-branch 12 5)))
 
-
-
-(display (total-weight a))
+;(display (balanced a))
+;(newline)
+;(display (balanced b))
+;(newline)
+;(display (balanced c))
+;(newline)
+(display (balanced d))
 (newline)
-(display (total-weight b))
-(newline)
-(display (total-weight c))
-(newline)
+;(display (total-weight a))
+;(newline)
+;(display (total-weight b))
+;(newline)
+;(display (total-weight c))
+;(newline)
 
